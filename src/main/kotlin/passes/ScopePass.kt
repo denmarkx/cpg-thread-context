29d834fd3b42830ca8ef2df7ae407c9f256886bc
Mark <frozenstranger510@gmail.com>
2025-11-17 20:15:44 -0600

    passes: name functionscopes

---
+++ src/main/kotlin/passes/ScopePass.kt
@@ -0,0 +1,21 @@
+package passes
+
+import de.fraunhofer.aisec.cpg.TranslationContext
+import de.fraunhofer.aisec.cpg.graph.declarations.TranslationUnitDeclaration
+import de.fraunhofer.aisec.cpg.graph.scopes.FunctionScope
+import de.fraunhofer.aisec.cpg.passes.TranslationUnitPass
+import de.fraunhofer.aisec.cpg.passes.configuration.ExecuteBefore
+import utils.AuxData
+import utils.Demangle
+
+@ExecuteBefore(LLVMThreadPass::class)
+class ScopePass(ctx: TranslationContext) : TranslationUnitPass(ctx) {
+    override fun cleanup() {}
+
+    override fun accept(t: TranslationUnitDeclaration) {
+        t.scope!!.children.filter { it is FunctionScope }.forEach { f ->
+            val name = Demangle.demangle(f.astNode!!.name.localName)
+            AuxData.addData(f, "name", name)
+        }
+    }
+}
