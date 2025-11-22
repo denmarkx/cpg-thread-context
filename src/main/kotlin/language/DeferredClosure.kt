d83fab93f33784bbab11a442765748aa297f8372
Mark <frozenstranger510@gmail.com>
2025-11-22 16:15:17 -0600

    language: psuedo-dfg for nodes moved into a closure

--- src/main/kotlin/language/DeferredClosure.kt
+++ src/main/kotlin/language/DeferredClosure.kt
@@ -3,6 +3,8 @@ package language
 
 import de.fraunhofer.aisec.cpg.graph.Node
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.Reference
+import graph.connectNodes
+import graph.setProperty
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -29,12 +31,32 @@ fun handleClosureCandidate(reference: Reference) {
 * Eventually, we expect the given VariableDeclaration to be used as a primary
 * argument to a CallExpression suffixed by {{closure}}.
 */
-class DeferredClosure(val reference: Reference, nodes: List<Node>) {
+class DeferredClosure(val reference: Reference, val nodes: List<Node>) {
     // todo: need to wait for an incoming call expression to a func ending with {{closure}} that uses reference as its only arg
 
     fun handle() {
-        print("handle defer closure candidate")
-        throw Exception("hdcc")
+        // The reference here is what is sent to the actual closure function.
+        // ..which is where nodes gets moved into via being reassigned to the reference.
+        /*
+        let x = 1234; (store i32 1234, i32* <spill>...)
+        let closure = move || { dbg!(x); }; (store i32 1234, i32* %closure,...)
+        closure(); // call i32 @"..."(i32* .... %closure)
+        */
+        nodes.forEach { n ->
+            connectNodes(n, reference, "DFG")
+        }
+    }
+
+    override fun toString() : String {
+        var nodeStr = "{\n"
+        nodes.forEach { n ->
+            nodeStr += "    " + n.name.localName + "\n"
+        }
+        nodeStr += "  }"
+
+        return "DeferredClosure {\n" +
+                "  reference = " + reference.name.localName + "\n" +
+                "  nodes = " + nodeStr + "\n}"
     }
 }
 
