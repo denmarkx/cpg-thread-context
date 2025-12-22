3a6e1af630be9f3da3fa09a2873db0b575c811e3
Mark <frozenstranger510@gmail.com>
2025-12-22 00:20:29 -0600

    passes: resolve select-case function pointers.

--- src/main/kotlin/passes/FunctionPtrResolver.kt
+++ src/main/kotlin/passes/FunctionPtrResolver.kt
@@ -3,30 +3,35 @@ package passes
 import de.fraunhofer.aisec.cpg.TranslationContext
 import de.fraunhofer.aisec.cpg.graph.Backward
 import de.fraunhofer.aisec.cpg.graph.GraphToFollow
-import de.fraunhofer.aisec.cpg.graph.calls
+import de.fraunhofer.aisec.cpg.graph.Node
+import de.fraunhofer.aisec.cpg.graph.declarations.Declaration
 import de.fraunhofer.aisec.cpg.graph.declarations.FunctionDeclaration
 import de.fraunhofer.aisec.cpg.graph.declarations.TranslationUnitDeclaration
-import de.fraunhofer.aisec.cpg.graph.edges.dataflows
+import de.fraunhofer.aisec.cpg.graph.declarations.VariableDeclaration
 import de.fraunhofer.aisec.cpg.graph.followDFGEdgesUntilHit
 import de.fraunhofer.aisec.cpg.graph.followPrevDFG
-import de.fraunhofer.aisec.cpg.graph.newCallExpression
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.CallExpression
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.Reference
 import de.fraunhofer.aisec.cpg.passes.SymbolResolver
 import de.fraunhofer.aisec.cpg.passes.TranslationUnitPass
 import de.fraunhofer.aisec.cpg.passes.configuration.DependsOn
-import graph.addLabel
-import graph.connectNodes
-import language.getTrueName
 
+/**
+ * Set of deferred function pointers where the CallExpression's LLVM instruction
+ * was determined to NOT satisfy LLVMIsAFunction.
+ *
+ * The elements are a Pair of the reference to the CallExpression and the CallExpression itself.
+*/
 val deferredFunctionPointers = mutableSetOf<Pair<Reference, CallExpression>>()
 
 /*
- * Function pointers aren't properly resolved. The library does this for
- * C++ through DynamicInvokeResolver, but not LLVM-IR.
+ * Function pointers aren't properly resolved. Currently, Inference.kt will make
+ * an attempt to create the function declarations. If function pointer is a parameter,
+ * there will be a disconnect between that register and the real ParameterDeclaration.
+ *
+ * The CPG library does this properly for C++ through DynamicInvokeResolver, but not LLVM-IR.
  *
  * This is a naive implementation.
- *
  * Does not support virtual dispatch.
 */
 @DependsOn(SymbolResolver::class)
@@ -34,31 +39,55 @@ class FunctionPtrResolver(ctx: TranslationContext) : TranslationUnitPass(ctx) {
     override fun cleanup() {}
 
     override fun accept(t: TranslationUnitDeclaration) {
-        // XXX: incorrect
+        // There exists a couple of oddities within the current inference and/or reference resolution:
+        // A.   In the case of the function pointer register being passed in as an argument,
+        //      there is no possible connection between that func ptr register and the new CallExpression.
+        //
+        // B.   Where the function pointer register is passed in as an argument AND stored in an intermediate register,
+        //      when that intermediate register is called, there exists a connection between the function pointer register
+        //      AND **A** ParameterDeclaration (or similar). However, this ParameterDeclaration, while apart of the same scope,
+        //      has no correlation to the function pointer.
+
+        // From the CallExpression, walk up the AST until we hit a FunctionDeclaration.
         deferredFunctionPointers.forEach {
-            val ref = it.first.refersTo ?: return@forEach
-            println("following ${ref.getTrueName()};(code=${ref.code}) dfg backwards until hit FuncDecl.")
-            ref.prevDFG.forEach { p ->
+            var astParent: Node? = it.second
+            while (astParent != null && astParent !is FunctionDeclaration) {
+                astParent = astParent.astParent
+            }
+
+            if (astParent == null) return@forEach
+
+            // From this FuncDecl, we are interested in the parameter that has the same code value as our reference.
+            // Since our actual call function pointer may be stored in another intermediate register,
+            // we walk the DFG backwards starting at OPERATOR_BASE until we get to
+            // a declaration node that is NOT VariableDeclaration. From that end node, the next DFG (who is a reference)
+            // will be the code from the arguments.
+
+            val nextDeclaration = it.second.operatorBase.followPrevDFG { n -> n is Declaration && n !is VariableDeclaration }
+            if (nextDeclaration == null) return@forEach
+
+            val validOperatorBase = nextDeclaration.nodes[nextDeclaration.nodes.size-2]
+            if (validOperatorBase !is Reference) return@forEach
+
+            val correspondingParam = astParent.parameters.find { p -> p.code == validOperatorBase.code }
+
+            // Normally, we would potentially say that callExpr - [INVOKES] -> correspondingParam..but
+            // Node.invokes expects a FuncDecl. Since it's ambiguious what callExpr can invoke,
+            // the logical thing to do here is to just point to all the possibilities.
+
+            // For this, starting at correspondingParam, follow the DFG backwards until
+            // it hits a Reference who refersTo a FunctionDeclaration.
+            correspondingParam?.prevDFG?.forEach { p ->
                 val paths = p.followDFGEdgesUntilHit(
                     findAllPossiblePaths = true,
                     direction = Backward(GraphToFollow.DFG),
-                    predicate = { n -> n is FunctionDeclaration }).fulfilled
+                    predicate = { n -> n is Reference && n.refersTo is FunctionDeclaration }).fulfilled
                 paths.forEach { np ->
-                    val last = np.nodes.last() as FunctionDeclaration
+                    val ref = np.nodes.last() as Reference
+                    val candidate = ref.refersTo as FunctionDeclaration
 
-                    println("  PATH:\n")
-                    for (node in np.nodes) {
-                        println("\t  NODE: ${node.getTrueName()}, CODE: ${node.code}\n")
-                    }
-
-                    it.second.invokes.add(last)
-                    it.second.invokeEdges.add(last)
-
-                    addLabel(last, "TEST_usedasfuncdecl")
-                    last.calledBy.add(it.second)
-                    last.calledByEdges.add(it.second.invokeEdges.last())
-                    last.nextDFG.add(it.second)
-                    last.nextDFGEdges.add(it.second)
+                    it.second.invokes.add(candidate)
+                    it.second.invokeEdges.add(candidate)
                 }
             }
         }
