6be3745ccd895a7de273c41f0e59758a93f35915
Mark <frozenstranger510@gmail.com>
2026-01-12 15:54:37 -0600

    lifetime: rustspec implementation for free call

--- src/main/kotlin/lving/backend/cpg/language/HeapLifetimeHandler.kt
+++ src/main/kotlin/lving/backend/cpg/language/HeapLifetimeHandler.kt
@@ -7,6 +7,7 @@ import de.fraunhofer.aisec.cpg.graph.*
 import de.fraunhofer.aisec.cpg.graph.declarations.Declaration
 import de.fraunhofer.aisec.cpg.graph.declarations.VariableDeclaration
 import de.fraunhofer.aisec.cpg.graph.statements.Statement
+import org.bytedeco.llvm.LLVM.LLVMAttributeRef
 import org.neo4j.ogm.annotation.Relationship
 
 // Function Attribute Enums and their corresponding values aren't specified in Bytedeco's bindings.
@@ -31,34 +32,35 @@ class HeapOperation : CallExpression() {
     @Relationship(value = "HEAP_DEALLOC", direction = Relationship.Direction.OUTGOING)
     val deallocNode = mutableListOf<Declaration>()
 
-    /**
-     * We're interested in knowing if this heap operation is within an exception handling block.
-     * If we don't appropriately mark this, then a double-free will become dominate in false positives.
-    */
-    var inCleanup : Boolean = false
-
     override var name : Name = Name("")
 }
 
 class HeapLifetimeHandler(val frontend: LLVMIRLanguageFrontend) : MetadataProvider {
 
     fun handle(call: LLVMValueRef, function: LLVMValueRef) : Statement? {
-        val attribute = attributeCache[function]?.find {
+        // See isLifetimeCall for info about this:
+        val attribute : LLVMAttributeRef? = attributeCache[function]?.find {
             LLVMIsEnumAttribute(it) > 0 &&
             LLVMGetEnumAttributeKind(it) == LLVMAttributeAllocaKind
-        } ?: return null
+        }
+
+        var operationKind: HeapOperations?
+        if (attribute != null) {
+            val attrValue = LLVMGetEnumAttributeValue(attribute)
+            operationKind = when {
+                (attrValue and LLVMAllocFnKindAlloc) >= 1L -> HeapOperations.ALLOC
+                (attrValue and LLVMAllocFnKindRealloc) >= 1L -> HeapOperations.REALLOC
+                (attrValue and LLVMAllocFnKindFree) >= 1L -> HeapOperations.FREE
+                else -> null
+            }
+        } else if (function.name.contains("drop_in_place")) {
+            operationKind = HeapOperations.FREE
+        } else return null
+
 
         val heapOperation = HeapOperation()
         heapOperation.applyMetadata(this)
-
-        val attrValue = LLVMGetEnumAttributeValue(attribute)
-        heapOperation.operation = when {
-            (attrValue and LLVMAllocFnKindAlloc) >= 1L -> HeapOperations.ALLOC
-            (attrValue and LLVMAllocFnKindRealloc) >= 1L -> HeapOperations.REALLOC
-            (attrValue and LLVMAllocFnKindFree) >= 1L -> HeapOperations.FREE
-            else -> null
-        }
-
+        heapOperation.operation = operationKind
         heapOperation.name = Name(heapOperation.operation.toString())
 
         // free(ptr, {size..}):
@@ -76,6 +78,11 @@ class HeapLifetimeHandler(val frontend: LLVMIRLanguageFrontend) : MetadataProvid
     fun isLifetimeCall(function: LLVMValueRef) : Boolean {
         // TODO: LLVMIsAFunction -> false for function pointers.
         if (LLVMIsAFunction(function) == null) return false
+
+        // RUSTSPEC: core::ptr::drop_in_place<T> will (at some point) lead to __rust_dealloc.
+        // which is tagged with a function attribute of allockind(...). Though I'm not
+        // interested in making more traversal hacks until we have a proper flow in the future.
+        if (function.name.contains("drop_in_place")) return true
 
         // The function itself may not have been parsed yet. MetadataExt keeps a lookup table
         // meaning if we parse attributes for a function here, we don't have to do that again later.
